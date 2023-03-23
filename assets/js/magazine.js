74a8e9bd12ea0a4d1f1f8e74f8c5561d61b50560
juliantello <95248642+juliantello@users.noreply.github.com>
2023-03-22 21:33:21 -0500

    Update magazine.js

--- assets/js/magazine.js
+++ assets/js/magazine.js
@@ -64,7 +64,7 @@
            pageElement.find('.loader').remove();
        });
 
-       let background_diferent = [5 , 6 , 7 , 8 , 10 , 11,12,13, 14 , 15,16,17,28,29,30,31,32,33,34,35,36,37,38,47,48,49]
+       let background_diferent = [3,5,  7, 8, 10,38,39,48,49]
 
        checkImage((background_diferent.includes(page))? 'assets/pics/backgrounds/' + page + '.png' : 'assets/pics/backgrounds/' + page + '.webp', img, pageElement, page)
        loadRegions(page, pageElement, lang);
