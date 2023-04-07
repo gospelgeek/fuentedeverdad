15ab512538054e568e7ba78813bbab110d3f2233
juliantello <yilver.tello@correounivalle.edu.co>
2023-04-07 08:49:26 -0500

    index Estados Unidos

--- assets/js/magazine.js
+++ assets/js/magazine.js
@@ -67,7 +67,7 @@
 
        let background_jpg = [37,50]
        if(page == 10){
-            video = $('<div/>', { 'class': 'videoPages' }).append($('<video />', { muted: "muted", id: "page-10-video", src: '/assets/pics/backgrounds/' + page + '.mp4', 'class': 'backVideo' + page }));
+            video = $('<div/>', { 'class': 'videoPages' }).append($('<video />', { muted: "muted", id: "page-10-video", src: 'assets/pics/backgrounds/' + page + '.mp4', 'class': 'backVideo' + page }));
             video.appendTo(pageElement)
        }else{
         checkImage((background_diferent.includes(page))? 'assets/pics/backgrounds/' + page + '.png' : (background_jpg.includes(page)) ?'assets/pics/backgrounds/' + page + '.jpg' : 'assets/pics/backgrounds/' + page + '.webp', img, pageElement, page)
